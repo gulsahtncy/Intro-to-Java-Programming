b379cef4ccb1331781d7a73462ba2fed1d1b73f5
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-09-24 22:11:30 +0300

    changing answer

--- chapter01/exercises/Exercise03.java
+++ chapter01/exercises/Exercise03.java
@@ -3,6 +3,6 @@ package chapter01.exercises;
 public class Exercise03 {
 	public static void main(String[] args) {
 		
-		System.out.println((10.5 + 2 * 3) / (45 - 4.5));		
+		System.out.println((10.5 + 2 * 3) / (45 - 3.5));		
 	}
 }
