5b4b9dee684f1d60477e334971e4423eafcb3e61
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-09-24 21:45:56 +0300

    Adding chapter02/exercises/Exercise01

---
+++ chapter02/exercises/Exercise01.java
@@ -0,0 +1,20 @@
+package chapter02.exercises;
+
+public class Exercise01 {
+	
+	public static void main(String[] args) {
+		double radius; // Declare radius
+		double area; // Declare area
+		
+		//Asign a radius
+		radius = 20; // radius is now 20
+		
+		//Compute area
+		area = radius * radius * 3.14159;
+		
+		//Display result
+		System.out.println("The area for the circle of radius " +
+				radius + " is " + area);
+	}
+
+}
