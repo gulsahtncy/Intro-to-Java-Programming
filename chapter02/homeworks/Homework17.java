330e5a063bb1697b25c5881b3a66b8e1c6318058
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-09-26 01:03:37 +0300

    Adding new homework

--- chapter02/homeworks/Homework17.java
+++ chapter02/homeworks/Homework17.java
@@ -1,5 +1,27 @@
 package chapter02.homeworks;
 
-public class Homework17 {
+import java.util.Scanner;
 
+public class Homework17 {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		//Science: wind-chill temperature
+		
+		System.out.print("Enter the temperature in Fahrenheit between -58°F and 41°F: ");
+		double fahrenheit = input.nextDouble();
+		
+		System.out.print("Enter the wind speed (>=2) in miles per hour: ");
+		double windSpeed = input.nextDouble();
+		
+		/*Wind-chill temperature = 35.74 + 0.6215 * degrees Fahrenheit - 
+		 * 35.75 * speed^0.16 + 0.4275 * degrees Fahrenheit * speed^0.16 
+		 */
+				
+		double windChill = 35.74 + (0.6215 * fahrenheit) - (35.75 * (Math.pow(windSpeed, 0.16))) +
+				0.4275 * fahrenheit * (Math.pow(windSpeed, 0.16));
+		
+		System.out.println("The wind chill index is " + (int)(windChill * 100000) / 100000.0);			
+	}
 }
