a71111479ebfbec2d29d583c49283f6879c4e506
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-09-27 00:35:42 +0300

    Adding new file

--- chapter02/homeworks/Homework22.java
+++ chapter02/homeworks/Homework22.java
@@ -1,5 +1,17 @@
 package chapter02.homeworks;
 
+import java.util.Scanner;
+
 public class Homework22 {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		//Financial application: monetary units
+		
+		System.out.println();
+		System.out.println();
+		
+	}
 
 }
