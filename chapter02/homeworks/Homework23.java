50b336896be97ec71adb3c967715aed226f78e96
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-09-27 00:37:16 +0300

    Adding new file

--- chapter02/homeworks/Homework23.java
+++ chapter02/homeworks/Homework23.java
@@ -1,5 +1,18 @@
 package chapter02.homeworks;
 
 public class Homework23 {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		//Cost of driving
+		
+		
+		
+		
+		
+		
+	}
+	
 
 }
