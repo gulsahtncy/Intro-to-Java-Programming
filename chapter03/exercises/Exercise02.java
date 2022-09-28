819f0cadd184b0e077f6cc05d1f53c2a9b6ce629
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-09-28 23:45:10 +0300

    Adding new file

---
+++ chapter03/exercises/Exercise02.java
@@ -0,0 +1,10 @@
+package chapter03.exercises;
+
+public class Exercise02 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 1_000_000; i++) {
+			System.out.println(System.currentTimeMillis());
+		}
+	}
+
+}
