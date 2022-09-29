25be909aee6ab65da7bf5f3363c42f483a2f3e73
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-09-29 23:45:38 +0300

    Adding new file

--- chapter03/exercises/Exercise01.java
+++ chapter03/exercises/Exercise01.java
@@ -4,25 +4,27 @@ import java.util.Scanner;
 
 public class Exercise01 {
 	public static void main(String[] args) {
-		Scanner klavye = new Scanner(System.in);
+			
+		//Addition Quiz
 		
-		System.out.println("Bir yil giriniz: ");
-		int yil = klavye.nextInt();
-		
-		boolean artikYilMi = yil % 4 == 0;
-		
-		artikYilMi = artikYilMi && yil % 100 != 0;
-		
-		artikYilMi = artikYilMi || yil % 400 == 0;
-		
-		// Yukaridakiyle ayni 
-		artikYilMi = (yil % 4 == 0 && yil % 100 != 0) || (yil % 400 == 0);
-		
-		if (artikYilMi) {
-			System.out.println(yil + " artik yildir");
-		} else {
-			System.out.println(yil + " artik yil degildir");
-		}
-	}
+		/* for (int i = 0; i < 10; i++) {
+		  		System.out.println(Math.random());
+		 }
+		 */ 
+		//0 <= Math.random() < 1	
+		//int number1 = (int)(Math.random() * 10);
+		//int number2 = (int)(Math.random() * 10);
 
+		int number1 = (int)(System.currentTimeMillis() % 10);
+		int number2 = (int)(System.currentTimeMillis() / 7 % 10);
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("What is " + number1 + " + " + number2 + "? ");
+		
+		int answer = input.nextInt();
+		
+		System.out.println(number1 + " + " + number2 + " = " + answer + " is " +
+				(number1 + number2 == answer));	
+	}
 }
--- chapter03/exercises/Exercise02.java
+++ chapter03/exercises/Exercise02.java
@@ -2,9 +2,27 @@ package chapter03.exercises;
 
 public class Exercise02 {
 	public static void main(String[] args) {
-		for (int i = 0; i < 1_000_000; i++) {
-			System.out.println(System.currentTimeMillis());
-		}
+	  
+		Scanner input = new Scanner(System.in);
+	   
+		System.out.println("Enter an integer: ");
+	   
+		int number = input.nextInt();
+
+	    if (number % 5 == 0)
+	      System.out.println("HiFive");
+
+	    if (number % 2 == 0)
+	      System.out.println("HiEven");
+	  }
 	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
