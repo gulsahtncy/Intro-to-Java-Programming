895f8e700210046eb2b18de4e29232014508ddbc
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-10-05 00:04:40 +0300

    new file

--- chapter03/homeworks/Homework03.java
+++ chapter03/homeworks/Homework03.java
@@ -8,19 +8,27 @@ public class Homework03 {
 		//Algebra: solve 2 * 2 linear equations
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Enter a, b, c, d, e, f: ");
-		int a = input.nextInt();
-		int b = input.nextInt();
-		int c = input.nextInt();
-		int d = input.nextInt();
-		int e = input.nextInt();
-		int f = input.nextInt();
-		
+		/*Cramer’s rule
+		 *ax + by = e      x = (ed - bf) / (ad - bc) 	  
+	     *cx + dy = f      y = (af - ec) / (ad - bc)         	     
+	     */
 		
 		System.out.println("Enter a, b, c, d, e, f: ");
+		double a = input.nextDouble();
+		double b = input.nextDouble();
+		double c = input.nextDouble();
+		double d = input.nextDouble();
+		double e = input.nextDouble();
+		double f = input.nextDouble();
 		
+		double x = (e * d - b * f) / (a * d - b * c);
+		double y = (a * f - e * c) / (a * d - b * c);
 		
-		
+		if ((a * d - b * c) != 0) {
+		System.out.println("x is " + x + " and y is " + y);		
+		}
+		else  {
+		System.out.println("The equation has no solution");	
+		}	
 	}
-
 }
