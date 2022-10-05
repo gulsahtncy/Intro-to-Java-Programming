23de9a49232829481395f2ca1b7a569ce95e8177
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-10-05 16:37:30 +0300

    Adding new file

--- chapter03/homeworks/Homework05.java
+++ chapter03/homeworks/Homework05.java
@@ -1,5 +1,40 @@
 package chapter03.homeworks;
 
-public class Homework05 {
+import java.util.Scanner;
 
+public class Homework05 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		//Find future dates
+		
+		System.out.println("Enter today's day(for example for sunday enter 0): "); 
+		int today = input.nextInt();
+		
+		System.out.println("Enter the number of days elapsed since today: ");
+		int nextDay = input.nextInt();
+
+		int nextDayNumber = (today + nextDay) % 7;
+		
+		switch (today) {
+		case 0: System.out.print("Today is Sunday and"); break;
+		case 1: System.out.print("Today is Monday and"); break;
+		case 2: System.out.print("Today is Thuesday and"); break;
+		case 3: System.out.print("Today is Wednesday and"); break;
+		case 4: System.out.print("Today is Thursday and"); break;
+		case 5: System.out.print("Today is Friday and"); break;
+		case 6: System.out.print("Today is Saturday and"); break;
+		}		
+		
+		switch (nextDayNumber) {
+		case 0: System.out.println(" the future day is Sunday"); break;
+		case 1: System.out.println(" the future day is Monday"); break;
+		case 2: System.out.println(" the future day is Thuesday"); break;
+		case 3: System.out.println(" the future day is Wednesday"); break;
+		case 4: System.out.println(" the future day is Thursday"); break;
+		case 5: System.out.println(" the future day is Friday"); break;
+		case 6: System.out.println(" the future day is Saturday"); break;
+		}				
+	} 
 }
+		
--- chapter03/homeworks/Homework06.java
+++ chapter03/homeworks/Homework06.java
@@ -1,5 +1,45 @@
 package chapter03.homeworks;
 
-public class Homework06 {
+import java.util.Scanner;
 
+public class Homework06 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		//Health application: BMI
+		
+		// Prompt the user to enter weight in pounds
+		System.out.print("Enter weight in pounds: ");
+		double pounds = input.nextDouble();
+
+		// Prompt the user to enter weight in feet
+		System.out.print("Enter feet: ");
+		double feet = input.nextDouble();
+
+		// Prompt the user to enter weight in inches
+		System.out.print("Enter inches: ");
+		double inches = input.nextDouble();
+
+		final double KILOGRAMS_PER_POUND = 0.45359237;
+		final double METERS_PER_INCH = 0.0254;
+		final double INCH_PER_FEET = 12;
+
+		// Compute BMI
+		double weightInKilograms = pounds * KILOGRAMS_PER_POUND;
+		double totalInches = (feet * INCH_PER_FEET) + inches;
+		double heightInMeters = totalInches * METERS_PER_INCH;
+		double bmi = weightInKilograms / (heightInMeters * heightInMeters);
+
+		// Display result
+		System.out.println("BMI is " + bmi);
+		if (bmi < 18.5) {
+			System.out.println("Underweight");
+		} else if (bmi < 25) {
+			System.out.println("Normal");
+		} else if (bmi < 30) {
+			System.out.println("Overweight");
+		} else {
+			System.out.println("Obese");
+		}
+	}
 }
