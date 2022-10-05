7702582cc1e831c0e38ba10e83e241cb38c1ed35
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-10-06 00:34:11 +0300

    Adding new file

--- chapter03/homeworks/Homework08.java
+++ chapter03/homeworks/Homework08.java
@@ -1,5 +1,37 @@
 package chapter03.homeworks;
 
-public class Homework08 {
+import java.util.Scanner;
 
-}
+public class Homework08 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		//Sort three integers
+		
+		int x = input.nextInt();
+		int y = input.nextInt();
+		int z = input.nextInt();
+		
+		if (x > y) {
+		 int temp1 = x;
+		 x = y;
+		 y = temp1;
+		} 
+		
+		if (y > z) {
+		 int temp2 = y;
+		 y = z;
+		 z = temp2;	
+		}
+		
+		if (x > y) {
+		 int temp3 = x;
+		 x = y;
+	     y = temp3;
+		}
+	
+		System.out.println(x + " " + y + " " + z);	
+	}		
+}			
+
+
