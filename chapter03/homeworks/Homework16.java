cb30164792cb280bf1ef630fb3250b6750619247
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-10-07 13:47:28 +0300

    Adding new file

--- chapter03/homeworks/Homework16.java
+++ chapter03/homeworks/Homework16.java
@@ -1,5 +1,21 @@
 package chapter03.homeworks;
 
-public class Homework16 {
+import java.util.Scanner;
 
+public class Homework16 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		//Random point
+		int x = (int) ((Math.random() * (50 + 50)) - 50);
+		int y = (int) ((Math.random() * (100 + 100)) - 100);
+		
+		System.out.println("Random coordinate points in a rectangle is " + 
+															  x + "," + y);
+		
+		
+		
+		
+	
+	}
 }
