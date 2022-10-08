8dfd3cbfa14fca03e54d0288c924138adb440456
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-10-08 22:13:41 +0300

    adding new f

--- chapter03/homeworks/Homework21.java
+++ chapter03/homeworks/Homework21.java
@@ -1,5 +1,13 @@
 package chapter03.homeworks;
 
+import java.util.Scanner;
+
 public class Homework21 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("a");
+	}
 
 }
+
