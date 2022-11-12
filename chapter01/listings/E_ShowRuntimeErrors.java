2c38581f61de91e4103d53bf80b55b6ba9237c33
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-11-12 17:40:04 +0300

    Adding new file

---
+++ chapter01/listings/E_ShowRuntimeErrors.java
@@ -0,0 +1,16 @@
+package chapter01.listings;
+
+public class E_ShowRuntimeErrors {
+	/*public static void main(String[] args) {
+	    System.out.println(1 / 0);} */
+	//The runtime error causes the program to terminate abnormally
+	
+	//ShowRuntimeErrors.java: Program contains runtime errors
+			public static void main(String[] args) {
+				System.out.println("Hatadan önce");
+				System.out.println(1 / 0);
+				System.out.println("sıfıra bölünmez"); 
+				} 
+}
+
+	
---
+++ chapter01/listings/F_ShowLogicErrors.java
@@ -0,0 +1,14 @@
+package chapter01.listings;
+
+public class F_ShowLogicErrors {
+	
+	/*System.out.println("Celsius 35 is Fahrenheit degree ");
+         System.out.println((9 / 5) * 35 + 32); */
+	
+	//ShowLogicErrors.java: Program contains logic errors
+	public static void main(String[] args) {
+		
+		System.out.println("Celsius 35 is Fahrenheit degree ");
+		System.out.println((9.0 / 5) * 35 + 32);
+	}
+}
