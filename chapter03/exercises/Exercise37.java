bc0f1f8d2db7eccb1579d8705c9ec76d2dafb505
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-11-17 17:53:18 +0300

    Adding new file

--- chapter03/exercises/Exercise37.java
+++ chapter03/exercises/Exercise37.java
@@ -1,5 +1,27 @@
 package chapter03.exercises;
 
 public class Exercise37 {
-
+	public static void main(String[] args) {
+		//The precedence of the the Boolean operators is (listed from high to low):
+		
+		/* NOT: !
+		 * XOR: ^
+		 * AND: &&
+		 * OR : || 
+		 */
+		
+		// The expression true || true && false will be evaluated as follows.
+		
+		/* true || (true && false)  
+		 * true || false
+		 * true
+		 */
+		
+		//The expression true && true || false will be evaluated as follows.
+		
+		/*(true && true) || false  
+		 * true || false  
+		 * true
+		 */	
+	}
 }
--- chapter03/exercises/Exercise40.java
+++ chapter03/exercises/Exercise40.java
@@ -1,5 +1,12 @@
 package chapter03.exercises;
 
 public class Exercise40 {
+	public static void main(String[] args) {
+		 
+		// (x > 0 && x < 10) is the same as ((x > 0) && (x < 10))
 
+		// (x > 0 || x < 10) is the same as ((x > 0) || (x < 10))
+
+		// (x > 0 || x < 10 && y < 0) is the same as (x > 0 || (x < 10 && y < 0))
+	}
 }
