79970e7fd88e789de5228917f059f69c03c623fb
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-11-19 23:51:59 +0300

    Adding new file

--- chapter04/exercises/Exercise06.java
+++ chapter04/exercises/Exercise06.java
@@ -2,4 +2,14 @@ package chapter04.exercises;
 
 public class Exercise06 {
 
+		//Why does the Math class not need to be imported?
+	
+		/* There is one package that is automatically imported 
+		 * into any Java program, this package is called java.lang.
+		 */
+
+		/* One of the classes that belongs to the java.lang package is 
+		 * the Math class. This means that the Math methods can be used 
+		 * directly without the need for explicit import.
+		 */
 }
--- chapter04/exercises/Exercise07.java
+++ chapter04/exercises/Exercise07.java
@@ -1,5 +1,10 @@
 package chapter04.exercises;
 
 public class Exercise07 {
-
+	public static void main(String[] args) {
+		System.out.println(Math.log(Math.exp(5.5)));
+		System.out.println(Math.exp(Math.log(5.5)));
+		System.out.println(Math.asin(Math.sin(Math.PI / 6)));
+		System.out.println(Math.sin(Math.asin(Math.PI / 6)));		
+	}
 }
