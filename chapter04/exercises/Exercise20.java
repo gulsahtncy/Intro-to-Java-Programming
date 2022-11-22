3e58958fd8827386a957137240c26e83b681c7f8
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-11-22 10:31:14 +0300

    Adding new file

--- chapter04/exercises/Exercise20.java
+++ chapter04/exercises/Exercise20.java
@@ -6,21 +6,36 @@ public class Exercise20 {
 		String s1 = " Welcome ";
 		String s2 = " welcome ";
 				
-		boolean isEqual = s1.equals(s2);
-		boolean isEqual = s1.equalsIgnoreCase(s2);
-		int x = s1.compareTo(s2);
-		int x = s1.compareToIgnoreCase(s2);  
-		boolean b = s1.startsWith("AAA");  
-		boolean b = s1.endsWith("AAA");  
-		int x = s1.length();  		
-		char x = s1.charAt(0);  		
-		String s3 = s1 + s2;  		
-		String s3 = s1.substring(1);  		
-		String s3 = s1.substring(1, 5);  		
-		String s3 = s1.toLowerCase();  		
-		String s3 = s1.toUpperCase();  		
-		String s3 = s1.trim();  		
-		int x = s1.indexOf('e');		
-		int x = s1.lastIndexOf("abc");		
+		boolean isEqual = s1.equals(s2); // a
+		
+		boolean isEqual = s1.equalsIgnoreCase(s2); // b
+		
+		int x = s1.compareTo(s2); // c
+		
+		int x = s1.compareToIgnoreCase(s2); // d
+		
+		boolean b = s1.startsWith("AAA"); // e
+		
+		boolean b = s1.endsWith("AAA"); // f  
+		
+		int x = s1.length(); // g  		
+		
+		char x = s1.charAt(0); // h  		
+		
+		String s3 = s1 + s2; // i  		
+		
+		String s3 = s1.substring(1); // j  		
+		
+		String s3 = s1.substring(1, 5); // k  		
+		
+		String s3 = s1.toLowerCase(); // l  		
+		
+		String s3 = s1.toUpperCase(); // m  		
+		
+		String s3 = s1.trim(); // n  		
+		
+		int x = s1.indexOf('e'); // o		
+		
+		int x = s1.lastIndexOf("abc"); // p		
 	}
 }
