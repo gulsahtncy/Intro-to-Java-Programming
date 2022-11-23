7cdf58d6112ee2167468e6f9869de8cbd277b93c
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-11-23 23:04:08 +0300

    Adding new file

--- chapter04/exercises/Exercise21.java
+++ chapter04/exercises/Exercise21.java
@@ -1,5 +1,10 @@
 package chapter04.exercises;
 
 public class Exercise21 {
-
+	public static void main(String[] args) {
+		
+		//We will get the answer by adding the first numbers in Set1, Set3, and Set4
+		
+		//1 + 4 + 8 = 13
+	}
 }
