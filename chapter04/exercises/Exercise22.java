514a24b5146b2b059d1a50117f576b9ff67fe2f8
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-11-24 23:42:06 +0300

    Adding new file

--- chapter04/exercises/Exercise22.java
+++ chapter04/exercises/Exercise22.java
@@ -1,5 +1,18 @@
 package chapter04.exercises;
 
 public class Exercise22 {
+	public static void main(String[] args) {
+		
+		/* What are the format specifiers for outputting a Boolean value, 
+		 * a character, a decimal integer, a floating-point number, and a string?
+		 */
+		
+		/* The format specifier for outputting a boolean is %b.
+		 * The format specifier for outputting a character is %c.
+		 * The format specifier for outputting a decimal is %d.
+		 * The format specifier for outputting a boolean is %f.
+		 * The format specifier for outputting a string is %s.
+		 */
 
+	}
 }
