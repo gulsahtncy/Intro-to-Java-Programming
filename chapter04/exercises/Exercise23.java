f7859724955a9c445319a5374fd84d6e871645ee
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-11-30 14:55:30 +0300

    Adding new file

--- chapter04/exercises/Exercise23.java
+++ chapter04/exercises/Exercise23.java
@@ -1,5 +1,16 @@
 package chapter04.exercises;
 
-public class Exercise23 {
-
+public class Exercise23 {	
+	
+	//System.out.printf("%5d %d\n", 1, 2, 3); 
+	//Erroneous code because there are three items to be printed but only two format specifiers.
+	
+	//System.out.printf("%5d %f", 1);
+	//Erroneous code because there are two format specifiers but only one item to be printed.
+	
+	//System.out.printf("%5d %f", 1, 2);
+	//
+	
+	
+	
 }
--- chapter04/exercises/Exercise24.java
+++ chapter04/exercises/Exercise24.java
@@ -1,5 +1,29 @@
 package chapter04.exercises;
 
 public class Exercise24 {
-
+	public static void main(String[] args) {
+		System.out.print("(a)"); 
+		System.out.printf("amount is %f %e\n", 32.32, 32.32);
+	    System.out.println();
+	    
+	    System.out.print("(b)");
+		System.out.printf("amount is %5.2f%% %5.4e\n", 32.327, 32.32);
+		System.out.println();
+		    
+		System.out.print("(c)");
+		System.out.printf("%6b\n", (1 > 2));
+		System.out.println();
+		    
+		System.out.print("(d)");
+		System.out.printf("%6s\n", "Java");
+		System.out.println();
+		    
+		System.out.print("(e)");
+		System.out.printf("%-6b%s\n", (1 > 2), "Java");
+		System.out.println();
+		    
+		System.out.print("(f)");
+		System.out.printf("%6b%-8s\n", (1 > 2), "Java");
+		System.out.println();		    
+	}
 }
