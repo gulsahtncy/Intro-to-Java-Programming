ffa9564f3021c263c173340dea63b95243b6e4d8
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-12-03 22:52:44 +0300

    Adding new f

--- chapter04/homeworks/Homework18.java
+++ chapter04/homeworks/Homework18.java
@@ -1,8 +1,51 @@
 package chapter04.homeworks;
 
+import java.util.Scanner;
+
 public class Homework18 {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
 		// Student major and status
-	}
 
+		System.out.print("Enter two characters: ");
+		String status = input.nextLine();
+		char major = status.charAt(0);
+		char grade = status.charAt(1);
+
+		String str = "";
+		switch (major) {
+		case 'M':
+			str = "Mathematics";
+			break;
+		case 'C':
+			str = "Computer Science";
+			break;
+		case 'I':
+			str = "Information Technology";
+			break;
+		default:
+			System.out.println("Invalid input.");
+			System.exit(0);
+		}
+		String cls = "";
+		switch (grade) {
+		case '1':
+			cls = "Freshman";
+			break;
+		case '2':
+			cls = "Sophomore";
+			break;
+		case '3':
+			cls = "Junior";
+			break;
+		case '4':
+			cls = "Senior";
+			break;
+		default:
+			System.out.println("Invalid input.");
+			System.exit(0);
+		}
+		System.out.println(str + " " + cls);
+	}
 }
