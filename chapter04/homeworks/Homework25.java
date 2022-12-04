0b74f6772573d62694a7819fe7eac41990ba1209
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-12-04 23:22:50 +0300

    Adding new file

--- chapter04/homeworks/Homework25.java
+++ chapter04/homeworks/Homework25.java
@@ -2,7 +2,14 @@ package chapter04.homeworks;
 
 public class Homework25 {
 	public static void main(String[] args) {
+		
 		// Generate vehicle plate numbers
+		
+		char ch1 = (char)((int)(Math.random() * 26 + 65));
+		char ch2 = (char)((int)(Math.random() * 26 + 65));
+		char ch3 = (char)((int)(Math.random() * 26 + 65));
+		int num = (int)(Math.random() * 9001 + 1000);
+		
+		System.out.println("The plate is " + ch1 + ch2 + ch3 + num);
 	}
-
 }
--- chapter04/homeworks/Homework26.java
+++ chapter04/homeworks/Homework26.java
@@ -1,8 +1,36 @@
 package chapter04.homeworks;
 
+import java.util.Scanner;
+
 public class Homework26 {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
 		// Financial application: monetary units
-	}
+		
+		System.out.print("Enter an amount in double, for example 11.56: ");
+		String amount = input.next();
+		
+		int k = amount.indexOf(',');
+		int numberOfOneDollars = Integer.parseInt(amount.substring(0, k));
+		int numberOfCents = Integer.parseInt(amount.substring(k + 1));
+		
+		int numberOfQuarters = numberOfCents / 25;
+		numberOfCents = numberOfCents % 25;
 
+		int numberOfDimes = numberOfCents / 10;
+		numberOfCents = numberOfCents % 10;
+
+		int numberOfNickels = numberOfCents / 5;
+		numberOfCents = numberOfCents % 5;
+
+		int numberOfPennies = numberOfCents;
+		
+		System.out.println("Your amount " + amount + " consists of");
+		System.out.println("    " + numberOfOneDollars + " dollars");
+		System.out.println("    " + numberOfQuarters + " quarters ");
+		System.out.println("    " + numberOfDimes + " dimes");
+		System.out.println("    " + numberOfNickels + " nickels");
+		System.out.println("    " + numberOfPennies + " pennies");
+	}
 }
