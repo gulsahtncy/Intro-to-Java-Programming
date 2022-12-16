9546f0daa3ec70f2705dd920cbb8e56a75784699
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-12-16 22:41:30 +0300

    Adding new file

---
+++ chapter05/exercises/Exercise09.java
@@ -0,0 +1,10 @@
+package chapter05.exercises;
+
+public class Exercise09 {
+	public static void main(String[] args) {
+		
+		for (int i = 1; i < 101; i++) {
+			System.out.println(i);			
+		}
+	}
+}
