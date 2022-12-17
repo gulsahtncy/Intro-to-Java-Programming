f8e5c580e9c8679a5e79fd1ed99b00be83cf71fa
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-12-17 21:57:41 +0300

    Adding new file

--- chapter05/exercises/Exercise15.java
+++ chapter05/exercises/Exercise15.java
@@ -2,4 +2,16 @@ package chapter05.exercises;
 
 public class Exercise15 {
 
+	/*
+	 * Can you convert a for loop to a while loop? List the advantages of using for
+	 * loops.
+	 */
+	
+	// for loop can be converted into a while loop.
+
+	// Some benefits of for loops are:
+
+	// A for certain type of problem, a for loop can be easier to construct and easier to read than the corresponding while loop.
+	// It is a very good (perhaps the best) choice for counting loops.
+	// All the control variables are nicely collected into one place when using for loops.
 }
