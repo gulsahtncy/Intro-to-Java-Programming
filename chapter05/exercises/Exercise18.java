4f7f01f241f4698cebb10b0025153c34f9bcc4fb
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-12-19 22:42:46 +0300

    Adding new file

--- chapter05/exercises/Exercise18.java
+++ chapter05/exercises/Exercise18.java
@@ -1,5 +1,15 @@
 package chapter05.exercises;
 
 public class Exercise18 {
+	public static void main(String[] args) {
 
+		int i = 0;
+		do {
+			System.out.println(i + 4);
+			i++;
+		} while (i < 10); // The problem with this code is a missing semicolon after the do-while loop.
+
+		for (int i = 0; i < 10; i++) // The problem with this code is that the semicolon is removed.
+			System.out.println(i + 4);
+	}
 }
