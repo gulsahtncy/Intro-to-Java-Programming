32be84ba90680469f8c19dd06c8d4d9babd00b23
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-12-22 17:20:07 +0300

    Adding new file

--- chapter05/listing/H_TestSum.java
+++ chapter05/listing/H_TestSum.java
@@ -1,5 +1,16 @@
 package chapter05.listing;
 
 public class H_TestSum {
-
+	public static void main(String[] args) {		
+	
+		// Initialize sum
+		double sum = 0; // change float in the program to double
+		
+		// Add 0.01, 0.02, ..., 0.99, 1 to sum
+		for (double i = 0.01f; i <= 1.0f; i = i + 0.01f) // change float in the program to double
+		sum += i;
+		
+		// Display result
+		System.out.println("The sum is " + sum);
+	}
 }
