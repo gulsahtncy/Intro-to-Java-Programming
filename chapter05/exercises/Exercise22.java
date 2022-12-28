279fde113d61a683df6f051a7f706131c50a7ff0
Gulsah Tuncay <gulsahtuncay1@gmail.com>
2022-12-28 23:07:29 +0300

    Adding new file

--- chapter05/exercises/Exercise22.java
+++ chapter05/exercises/Exercise22.java
@@ -1,5 +1,19 @@
 package chapter05.exercises;
 
 public class Exercise22 {
-
+	
+	/*The code will not compile if we remove the cast. 
+	 * This is because the code is semi dangerous since information might be 
+	 * lost when we convert an int to a char. An int can hold a wider range of 
+	 * numbers than a char.
+	 */
+	
+	/*But no information will be lost in this case because the numbers are 
+	 * always small enough to fit in a char.
+	 */
+	
+	/*So there is no problem with our code but we still need to make a cast 
+	 * to a char to indicate to compiler that we are aware of what we are doing.
+	 */
+	
 }
