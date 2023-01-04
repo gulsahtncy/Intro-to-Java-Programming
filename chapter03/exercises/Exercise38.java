31623a6dd65a05c762a848abbc5f49c4cd634a33
Gülşah <gulsahtuncay1@gmail.com>
2023-01-04 18:20:55 +0300

    Adding new file

--- chapter03/exercises/Exercise38.java
+++ chapter03/exercises/Exercise38.java
@@ -1,5 +1,10 @@
 package chapter03.exercises;
 
 public class Exercise38 {
-
+	public static void main(String[] args) {
+		
+		/* It is true that all the binary operators except 
+		*  =, +=, -=, *=, /=, %= are left associative
+		*/
+	}
 }
--- chapter03/exercises/Exercise39.java
+++ chapter03/exercises/Exercise39.java
@@ -1,5 +1,20 @@
 package chapter03.exercises;
 
 public class Exercise39 {
-
+	public static void main(String[] args) {
+		
+		/* 2 * 2 - 3 > 2 && 4 – 2 > 5  
+		 * 4 - 3 > 2 && 4 - 2 > 5
+		 * 1 > 2 && 2 > 5 
+		 * false && false 
+		 * false
+		 */
+		
+		/* 2 * 2 - 3 > 2 || 4 – 2 > 5  
+		 * 4 - 3 > 2 || 4 – 2 > 5  
+		 * 1 > 2 || 2 > 5  
+		 * false || false  
+		 * false 
+		 */
+	}
 }
