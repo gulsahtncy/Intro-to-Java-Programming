adbf15ce5b1d50d1bba6868aabce8630a64f886d
Gülşah <gulsahtuncay1@gmail.com>
2023-01-05 22:25:50 +0300

    Adding new file

--- chapter03/homeworks/Homework21.java
+++ chapter03/homeworks/Homework21.java
@@ -5,52 +5,51 @@ import java.util.Scanner;
 public class Homework21 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+
 		// h = (q + (26 * (m + 1) /10) + k + (k / 4) + (j / 4) + 5 * j ) % 7
-		
+
 		System.out.println("Enter year:");
 		int year = input.nextInt();
-		
+
 		System.out.println("Enter month:");
 		int month = input.nextInt(); // m
-		
+
 		System.out.println("Enter the day of the month:");
 		int theDayOfTheMonth = input.nextInt(); // q
-		
+
 		int j = year / 100; // j is the century
 		int k = year % 100; // k is the year of the century
-		
-		int h = (theDayOfTheMonth + (26 * (month + 1) / 10) + k + (k / 4) +
-				(j / 4) + 5 * j) % 7;
-		
+
+		int h = (theDayOfTheMonth + (26 * (month + 1) / 10) + k + (k / 4) + (j / 4) + 5 * j) % 7;
+
 		String day = "";
-			
+
 		switch (h) {
-		case 0:		
+		case 0:
 			// System.out.print("Saturday");
 			day = "Saturday";
 			break;
-		case 1:		
+		case 1:
 			// System.out.print("Sunday");
 			day = "Sunday";
 			break;
-		case 2:		
+		case 2:
 			// System.out.print("Monday");
 			day = "Monday";
 			break;
-		case 3:		
+		case 3:
 			// System.out.print("Tuesday");
 			day = "Tuesday";
 			break;
-		case 4:		
+		case 4:
 			// System.out.print("Wednesday");
 			day = "Wednesday";
 			break;
-		case 5:		
+		case 5:
 			// System.out.print("Thursday");
 			day = "Thursday";
 			break;
-		case 6:		
+		case 6:
 			// System.out.print("Friday");
 			day = "Friday";
 			break;
@@ -58,4 +57,3 @@ public class Homework21 {
 		System.out.print("Day of the week is " + day);
 	}
 }
-
