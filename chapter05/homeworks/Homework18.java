7f385fe9c416d697dc430c9f628bcf06cf03a2f5
Gülşah <gulsahtuncay1@gmail.com>
2023-01-11 22:11:41 +0300

    Adding new file

--- chapter05/homeworks/Homework18.java
+++ chapter05/homeworks/Homework18.java
@@ -1,5 +1,19 @@
 package chapter05.homeworks;
 
 public class Homework18 {
+	public static void main(String[] args) {
 
+		System.out.println("Pattern D");
+		for (int i = 1; i < 7; i++) {
+			for (int s = 0; s < i; s++) {
+				System.out.print("  ");
+			}
+			for (int a = 1; a <= 7 - i; a++) {
+				System.out.print(a + " ");
+			}
+			System.out.println();
+
+		}
+
+	}
 }
---
+++ chapter05/homeworks/Homework19.java
@@ -0,0 +1,5 @@
+package chapter05.homeworks;
+
+public class Homework19 {
+
+}
