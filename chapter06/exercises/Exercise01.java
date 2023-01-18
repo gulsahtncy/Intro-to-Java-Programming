e9ff1357b832a61530e98ca9de969b036f127f3a
Gülşah <gulsahtuncay1@gmail.com>
2023-01-18 17:26:08 +0300

    Adding new file

--- chapter06/exercises/Exercise01.java
+++ chapter06/exercises/Exercise01.java
@@ -2,4 +2,12 @@ package chapter06.exercises;
 
 public class Exercise01 {
 
+	// What are the benefits of using a method?
+
+	   // Some of the benefits of using methods are:
+
+		// Faster development time by reuse of code
+		// Reduces complexity of the code
+		// Makes the code easier to maintain
+
 }
