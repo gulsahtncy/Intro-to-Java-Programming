289524a7e734bde45cf2599f73f4bdee0e855351
Gülşah <gulsahtuncay1@gmail.com>
2023-01-18 17:52:56 +0300

    Adding new file

--- chapter06/exercises/Exercise03.java
+++ chapter06/exercises/Exercise03.java
@@ -1,5 +1,16 @@
 package chapter06.exercises;
 
 public class Exercise03 {
+	public static void main(String[] args) {
+		int i = 5;
+		int j = 2;
+		int k = max(i, j);
+		System.out.println("The maximum of " + i + " and " + j + " is " + k);
+	}
+
+	/** Return the max of two numbers */
+	public static int max(int num1, int num2) {
+		return ( num1 > num2 ) ? num1 : num2;
+	}
 
 }
