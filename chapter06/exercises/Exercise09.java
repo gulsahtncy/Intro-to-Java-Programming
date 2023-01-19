e73fb82de95da7a17fe2a26445aecd3ffb3be432
Gülşah <gulsahtuncay1@gmail.com>
2023-01-19 16:14:23 +0300

    Adding new file

--- chapter06/exercises/Exercise09.java
+++ chapter06/exercises/Exercise09.java
@@ -1,5 +1,18 @@
 package chapter06.exercises;
 
 public class Exercise09 {
+	public static void method1(int n, int m) {
+		n += m;
+		method2(3.4);
+	}
+	
+	public static int method2(double n) {
+		if (n > 0)
+			return 1;
+		else if (n == 0)
+			return 0;
+		else 
+			return -1;
 
+	}		
 }
