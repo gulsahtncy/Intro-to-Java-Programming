ec3e132a47feee14903ad57098c7d6c3acb4dd05
Gülşah <gulsahtuncay1@gmail.com>
2023-01-27 14:49:56 +0300

    Adding new file

--- chapter06/exercises/Exercise18.java
+++ chapter06/exercises/Exercise18.java
@@ -1,5 +1,13 @@
 package chapter06.exercises;
 
 public class Exercise18 {
+	
+	// What is a local variable? 
+
+	/*
+	 * A local variable in Java is a variable that is declared within the body of a
+	 * method. This variable can be used variable only within that method. Other
+	 * methods in the class are not even aware that the variable exists.
+	 */
 
 }
--- chapter06/exercises/Exercise19.java
+++ chapter06/exercises/Exercise19.java
@@ -1,5 +1,12 @@
 package chapter06.exercises;
 
 public class Exercise19 {
+	
+	// What is the scope of a local variable? 
+
+	/*
+	 * The scope of a local variable starts from its declaration and continues to
+	 * the end of the block that contains the variable.
+	 */
 
 }
