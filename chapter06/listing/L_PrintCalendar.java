e4a7597d92c8fa38553c8901137e3b08ec468fe7
Gülşah <gulsahtuncay1@gmail.com>
2023-02-01 23:15:03 +0300

    Adding new file

---
+++ chapter06/listing/L_PrintCalendar.java
@@ -0,0 +1,8 @@
+package chapter06.listing;
+
+public final class L_PrintCalendar {
+	public static void main(String[] args) {
+		
+	
+	}
+}
--- chapter06/listing/L_TestRandomCharacter.java
+++
@@ -1,9 +0,0 @@
-package chapter06.listing;
-
-public final class L_TestRandomCharacter {
-	public static void main(String[] args) {
-		int a = 5;
-		System.out.println(a);
-	
-	}
-}
