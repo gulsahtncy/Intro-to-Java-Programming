92f572ff979fdd29ab0a2d6929b6b76b77d1e606
Gülşah <gulsahtuncay1@gmail.com>
2023-02-12 23:57:44 +0300

    Adding new file

--- chapter07/exercises/Exercise03.java
+++ chapter07/exercises/Exercise03.java
@@ -1,5 +1,12 @@
 package chapter07.exercises;
 
 public class Exercise03 {
-
+	public static void main(String[] args) {
+		
+		int x = 30;
+		int[] numbers = new int[x];
+		
+		System.out.println("x is " + x);
+		System.out.println("The size of numbers is " + numbers.length);
+	}
 }
