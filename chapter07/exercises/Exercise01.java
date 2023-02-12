3a8a521c47136813d1b38bd75baa8c37f94e848e
Gülşah <gulsahtuncay1@gmail.com>
2023-02-12 23:52:13 +0300

    Adding new file

--- chapter07/exercises/Exercise01.java
+++ chapter07/exercises/Exercise01.java
@@ -1,5 +1,26 @@
 package chapter07.exercises;
 
 public class Exercise01 {
+	
+	/*
+	 * An array reference variable is declared by the use of brackets. For example,
+	 * the following code declares a variable myList that references an array of
+	 * double elements: double[] myList;
+	 */
+
+	/*
+	 * An array reference variable is used as a handle to access the content of the
+	 * array, but just creating this variable will not allocate any space in memory
+	 * for the array. Memory for the array is allocated by using the keyword new. If
+	 * we need to be able to store 10 doubles so shall we write like in the
+	 * following code: myList = new double[10];
+	 */
+
+	/*
+	 * If we know the values that shall go in the array before it is created so is
+	 * it also possible to use a shorthand notation. This is called an array
+	 * initializer that will allocate memory and create the array reference:
+	 * double[] myList = {1.9, 2.4, 3.4, 3.5, 1.0, 5.7, 3.1, 1.1, 8.7, 4.6};
+	 */
 
 }
--- chapter07/exercises/Exercise02.java
+++ chapter07/exercises/Exercise02.java
@@ -1,5 +1,9 @@
 package chapter07.exercises;
 
 public class Exercise02 {
-
+	
+	/*
+	 * The memory needed for an array is allocated when the array is created, by the
+	 * use of the new keyword or using an array initializer.
+	 */
 }
