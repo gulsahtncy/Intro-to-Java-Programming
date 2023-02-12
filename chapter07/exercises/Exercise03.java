e1e4425968c28588646784d7a54f313f601c0a88
Gülşah <gulsahtuncay1@gmail.com>
2023-02-13 00:08:05 +0300

    Adding new file

--- chapter07/exercises/Exercise03.java
+++ chapter07/exercises/Exercise03.java
@@ -5,8 +5,10 @@ public class Exercise03 {
 		
 		int x = 30;
 		int[] numbers = new int[x];
-		
+		x = 60;
 		System.out.println("x is " + x);
 		System.out.println("The size of numbers is " + numbers.length);
+		
+		// The size of an array can not be changed after it is set.
 	}
 }
