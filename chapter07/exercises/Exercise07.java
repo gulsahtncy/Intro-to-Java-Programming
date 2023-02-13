8a1ab11614aefff2ce7318c13cf91895ec0815d9
Gülşah <gulsahtuncay1@gmail.com>
2023-02-13 14:54:11 +0300

    Adding new file

--- chapter07/exercises/Exercise07.java
+++ chapter07/exercises/Exercise07.java
@@ -2,4 +2,10 @@ package chapter07.exercises;
 
 public class Exercise07 {
 
+	// The type of an array index is int.
+
+	// The lowest index of an array is 0.
+
+	// The third element of an array named a is represented by a[2].
+
 }
--- chapter07/exercises/Exercise08.java
+++ chapter07/exercises/Exercise08.java
@@ -1,5 +1,32 @@
 package chapter07.exercises;
 
 public class Exercise08 {
+	public static void main(String[] args) {
+		
+		// Write statements to do the following: 
+			// a. Create an array to hold 10 double values. 
+		double[] listA = new double[10];
+			// b. Assign the value 5.5 to the last element in the array. 
+		listB[listB.length() - 1] = 5.5;
+			// c. Display the sum of the first two elements. 
+		System.out.println(listC[0] + listC[1]);
+			// d. Write a loop that computes the sum of all elements in the array.
+		int sum = 0;
+		for (int e : listD) {
+			sum += e;
+		}
+			// e. Write a loop that finds the minimum element in the array.
+		int min = listE[0];
+		for (int e : listE) {
+			if (e < min) {
+				min = e;
+			}
+		}
+			// f. Randomly generate an index and display the element of this index in the array.
+		System.out.println(listF[(int)(Math.random()*listF.length)]);
+			// g. Use an array initializer to create another array with the initial values 3.5, 5.5, 4.52, and 5.6.
+		double[] ListG = {3.5, 5.5, 4.52, 5.6};
+				
+	}
 
 }
