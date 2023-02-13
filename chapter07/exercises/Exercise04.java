af8ada1c6615fa411eb7f1f6207fe28572d012be
Gülşah <gulsahtuncay1@gmail.com>
2023-02-13 10:45:15 +0300

    Adding new file

--- chapter07/exercises/Exercise04.java
+++ chapter07/exercises/Exercise04.java
@@ -1,5 +1,11 @@
 package chapter07.exercises;
 
 public class Exercise04 {
+	
+	/*Indicate true or false for the following statements: 
+		■ Every element in an array has the same type. (TRUE)
+		■ The array size is fixed after an array reference variable is declared. (FALSE)
+		■ The array size is fixed after it is created. (TRUE) 
+		■ The elements in an array must be a primitive data type. (FALSE) */
 
 }
