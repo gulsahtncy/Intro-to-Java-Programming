46998668c66b8164e66d657f42fb6fcd50a37fb8
Gülşah <gulsahtuncay1@gmail.com>
2023-02-13 16:52:03 +0300

    Adding new file

--- chapter07/exercises/Exercise11.java
+++ chapter07/exercises/Exercise11.java
@@ -5,7 +5,7 @@ public class Exercise11 {
 		
 		int list[] = { 1, 2, 3, 4, 5, 6 };
 		for (int i = 1; i < list.length; i++)
-			list[i] = list[i - 1]; // 
+			list[i] = list[i - 1]; // 1 1 1 1 1 1 
 
 		for (int i = 0; i < list.length; i++) // 1 2 3 4 5 6 
 			System.out.print(list[i] + " ");
