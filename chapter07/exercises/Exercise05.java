684b072677edcaa6e5d51d936185812f321cdb36
Gülşah <gulsahtuncay1@gmail.com>
2023-02-13 10:49:58 +0300

    Adding new file

--- chapter07/exercises/Exercise05.java
+++ chapter07/exercises/Exercise05.java
@@ -1,5 +1,19 @@
 package chapter07.exercises;
 
 public class Exercise05 {
-
+	public static void main(String[] args) {
+		
+		int i = new int(30); //(FALSE)
+		
+		double d[] = new double[30]; //(TRUE)
+		
+		char[] r = new char(1..30); //(FALSE)
+		
+		int i[] = (3, 4, 3, 2); //(FALSE)
+		
+		float f[] = {2.3, 4.5, 6.6}; //(TRUE)
+		
+		char[] c = new char(); //(FALSE)
+					
+	}
 }
--- chapter07/exercises/Exercise06.java
+++ chapter07/exercises/Exercise06.java
@@ -1,5 +1,13 @@
 package chapter07.exercises;
 
 public class Exercise06 {
+	public static void main(String[] args) {		
+	// How do you access elements in an array?
+	
+	int i[] = {1, 5, 8, 2};
+	int myInt = i[1];	// myInt becomes 5
+	System.out.println(myInt);
 
+
+	}
 }
