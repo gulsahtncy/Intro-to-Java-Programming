ee04920f26dac21d137054d64b08fb67ccbc59d4
Gülşah <gulsahtuncay1@gmail.com>
2023-02-14 18:49:11 +0300

    Adding new file

--- chapter07/exercises/Exercise13.java
+++ chapter07/exercises/Exercise13.java
@@ -1,5 +1,11 @@
 package chapter07.exercises;
 
 public class Exercise13 {
-
+	public static void main(String[] args) {
+		
+		// The following code will copy the content of the array called "source" to the other array called "target".
+		int[] source = { 3, 4, 5 };
+		int[] target = new int[source.length];
+		System.arraycopy(source, 0, target, 0, source.length);
+	}
 }
--- chapter07/exercises/Exercise14.java
+++ chapter07/exercises/Exercise14.java
@@ -1,5 +1,13 @@
 package chapter07.exercises;
 
 public class Exercise14 {
+	public static void main(String[] args) {
 
+		// The following code will not really resize the array.
+
+		int[] myList;
+		myList = new int[10];
+		// Sometime later you want to assign a new array to myList
+		myList = new int[20];
+	}
 }
