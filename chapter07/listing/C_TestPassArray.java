67566578b2768aeb8488b452a04879b262ff193c
Gülşah <gulsahtuncay1@gmail.com>
2023-02-17 23:49:47 +0300

    Adding new file

--- chapter07/listing/C_TestPassArray.java
+++ chapter07/listing/C_TestPassArray.java
@@ -2,6 +2,24 @@ package chapter07.listing;
 
 public class C_TestPassArray {
 	public static void main(String[] args) {
+		int x = 1; // x represents an int value
+		int[] y = new int[10]; // y represents an array of int values
+		String a = "gülşah";
+		char k = a.charAt(0);
+		
+		m(x, y, a); // Invoke m with arguments x and y
+		System.out.println(k); 
+		
+		System.out.println("x is " + x);
+		System.out.println("y[0] is " + y[0]);
+		
+		
+	}
+
+	public static void m(int number, int[] numbers, String a) {
+		number = 1001; // Assign a new value to number
+		numbers[0] = 5555; // Assign a new value to numbers[0]
+		a = "Hüseyin";
 		
 	}
 
