67400490a884572c5013aac934d08f26655f8eb9
Gülşah <gulsahtuncay1@gmail.com>
2023-02-20 10:36:34 +0300

    Adding new file

--- chapter07/exercises/Exercise15.java
+++ chapter07/exercises/Exercise15.java
@@ -1,5 +1,13 @@
 package chapter07.exercises;
 
 public class Exercise15 {
-
+	public static void main(String[] args) {
+		int[] list = { 1, 2, 3, 5, 4 };
+		for (int i = 0, j = list.length - 1; i < list.length / 2; i++, j--) {
+			// Swap list[i] with list[j]
+			int temp = list[i];
+			list[i] = list[j];
+			list[j] = temp;
+		}
+	}
 }
