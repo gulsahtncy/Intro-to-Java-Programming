f96d1496936c762a5618de05bb7dbdcd736f7720
Gülşah <gulsahtuncay1@gmail.com>
2023-02-24 18:52:59 +0300

    Adding new file

--- chapter07/exercises/Exercise19.java
+++ chapter07/exercises/Exercise19.java
@@ -2,4 +2,20 @@ package chapter07.exercises;
 
 public class Exercise19 {
 
+	// Each of the following code snippets are incorrect.
+
+	/*
+	 * public static void print(String... strings, double... numbers) The problem
+	 * with above code is that it is only allowed to have a single variable-length
+	 * parameter.
+	 * 
+	 * public static void print(double... numbers, String name) The problem with the
+	 * above is that a variable-length parameter is only allowed to be placed last
+	 * int the parameter list.
+	 * 
+	 * public static double... print(double d1, double d2) The problem with the
+	 * above code is that it is not allowed to return a variable-length type from a
+	 * method.
+	 */
+
 }
