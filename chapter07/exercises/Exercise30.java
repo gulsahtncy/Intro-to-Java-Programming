4c3eb50d3524fab446e917323d440a068be05d46
Gülşah <gulsahtuncay1@gmail.com>
2023-06-17 15:21:28 +0300

    Adding new fi

--- chapter07/exercises/Exercise30.java
+++ chapter07/exercises/Exercise30.java
@@ -1,15 +1,13 @@
 package chapter07.exercises;
 
 public class Exercise30 {
-	
-	java Exercise30 I have a dream
-	//java Exercise30 “1 2 3”
-	//java Exercise30
-
 	public static void main(String[] args) {
 		System.out.println("Number of strings is " + args.length);
 		for (int i = 0; i < args.length; i++)
 			System.out.println(args[i]);
 
+		// 1. java Test I have a dream //Number of strings is 4
+		// 2. java Test “1 2 3” //Number of strings is 1
+		// 3. java Test //Number of strings is 0
 	}
 }
