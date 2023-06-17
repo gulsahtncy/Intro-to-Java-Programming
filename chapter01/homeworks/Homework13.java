e9bf02edc43a6e024e9213bc39fff7ab7bd30fe4
Gülşah <gulsahtuncay1@gmail.com>
2023-06-17 16:07:31 +0300

    Adding new file

--- chapter01/homeworks/Homework13.java
+++ chapter01/homeworks/Homework13.java
@@ -13,6 +13,11 @@ public class Homework13 {
 		System.out.println(((44.5 * .55) - (50.2 * 5.9)) / ((3.4 * .55) - (50.2 * 2.1)));
 		System.out.print("y = ");
 		System.out.println(((3.4 * 5.9) - (44.5 * 2.1)) / ((3.4 * .55) - (50.2 * 2.1)));
+		
+		System.out.print("x = ");
+		System.out.println(((44.5 * .55) - (50.2 * 5.9)) / ((3.4 * .55) - (50.2 * 2.1)));
+		System.out.print("y = ");
+		System.out.println(((3.4 * 5.9) - (44.5 * 2.1)) / ((3.4 * .55) - (50.2 * 2.1)));
 	}
 }
 
