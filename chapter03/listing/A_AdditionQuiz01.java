6504c4a1ee23db8734bffde8d994f320ad9c2aac
Gülşah <gulsahtuncay1@gmail.com>
2023-07-05 23:56:57 +0300

    Adding new file

--- chapter03/listing/A_AdditionQuiz01.java
+++ chapter03/listing/A_AdditionQuiz01.java
@@ -4,29 +4,26 @@ import java.util.Scanner;
 
 public class A_AdditionQuiz01 {
 	public static void main(String[] args) {
-		
-		//Addition Quiz
-		
-		/* for (int i = 0; i < 10; i++) {
-		  		System.out.println(Math.random());
-		 } */
-		  
-		//0 <= Math.random() < 1	
-		//int number1 = (int)(Math.random() * 10);
-		//int number2 = (int)(Math.random() * 10);
 
-		int number1 = (int)(System.currentTimeMillis() % 10);
-		int number2 = (int)(System.currentTimeMillis() / 7 % 10);
-		
+		// Addition Quiz
+
+		/*
+		 * for (int i = 0; i < 10; i++) { System.out.println(Math.random()); }
+		 */
+
+		// 0 <= Math.random() < 1
+		// int number1 = (int)(Math.random() * 10);
+		// int number2 = (int)(Math.random() * 10);
+
+		int number1 = (int) (System.currentTimeMillis() % 10);
+		int number2 = (int) (System.currentTimeMillis() / 7 % 10);
+
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.print("What is " + number1 + " + " + number2 + "? ");
-		
+
 		int answer = input.nextInt();
-		
-		System.out.println(number1 + " + " + number2 + " = " + answer + " is " +
-				(number1 + number2 == answer));	
+
+		System.out.println(number1 + " + " + number2 + " = " + answer + " is " + (number1 + number2 == answer));
 	}
 }
-
-
--- chapter03/listing/B_SimpleIfDemo02.java
+++ chapter03/listing/B_SimpleIfDemo02.java
@@ -4,21 +4,20 @@ import java.util.Scanner;
 
 public class B_SimpleIfDemo02 {
 	public static void main(String[] args) {
-		
-		   //Simple If Demo
-			
-			Scanner input = new Scanner(System.in);
 
-		    System.out.println("Enter an integer: ");
-		    int number = input.nextInt();
+		// Simple If Demo
 
-		    if (number % 5 == 0) {
-		      System.out.println("Hi Five");	      
-		    }
-		   
-		    if (number % 2 == 0) {
-		      System.out.println("Hi Even");
-		    }
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter an integer: ");
+		int number = input.nextInt();
+
+		if (number % 5 == 0) {
+			System.out.println("Hi Five");
 		}
-	}	
 
+		if (number % 2 == 0) {
+			System.out.println("Hi Even");
+		}
+	}
+}
