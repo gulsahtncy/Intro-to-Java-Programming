a80d33ec85e9a413cbce1318ab8ba00631d938e8
Gülşah <gulsahtuncay1@gmail.com>
2023-07-09 23:34:16 +0300

    Adding new file

--- chapter03/exercises/Exercise09.java
+++ chapter03/exercises/Exercise09.java
@@ -3,34 +3,16 @@ package chapter03.exercises;
 public class Exercise09 {
 	public static void main(String[] args) {
 
-		int x = 2;
-		int y = 3;
-		
-		if (x > 2)
+		int x = 3;
+		int y = 2;
+
+		if (x > 2) {
 			if (y > 2) {
 				int z = x + y;
 				System.out.println("z is " + z);
-			} else
-				System.out.println("x is " + x);
-		
-		x = 3;
-		y = 2;
-		
-		if (x > 2)
-			if (y > 2) {
-				int z = x + y;
-				System.out.println("z is " + z);
-			} else
-				System.out.println("x is " + x);
-		
-		x = 3;
-		y = 3;
-		
-		if (x > 2)
-			if (y > 2) {
-				int z = x + y;
-				System.out.println("z is " + z);
-			} else
-				System.out.println("x is " + x);
+			}
+		} else
+			System.out.println("x is " + x);
+
 	}
 }
