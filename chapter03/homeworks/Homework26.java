9649579314753364a0dd20072cd0c27342b2c18d
Gülşah <gulsahtuncay1@gmail.com>
2023-07-11 23:24:49 +0300

    Adding new file

--- chapter03/homeworks/Homework26.java
+++ chapter03/homeworks/Homework26.java
@@ -1,5 +1,22 @@
 package chapter03.homeworks;
 
-public class Homework26 {
+import java.util.Scanner;
 
+public class Homework26 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		// Use the &&, || and ^ operators
+
+		System.out.println("Enter an integer: ");
+		int number = input.nextInt();
+
+		boolean condition1 = (number % 5 == 0) && (number % 6 == 0);
+		boolean condition2 = (number % 5 == 0) || (number % 6 == 0);
+		boolean condition3 = ((number % 5 == 0) && (number % 6 == 0)) ^ ((number % 5 == 0) || (number % 6 == 0));
+
+		System.out.println("Is " + number + " divisible by 5 and 6? " + condition1);
+		System.out.println("Is " + number + " divisible by 5 or 6? " + condition2);
+		System.out.println("Is " + number + " divisible by 5 or 6, but not both? " + condition3);
+	}
 }
