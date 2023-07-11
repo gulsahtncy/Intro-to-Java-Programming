58f5fc8a6c49db0202684bb7d90ed44b19c0736a
Gülşah <gulsahtuncay1@gmail.com>
2023-07-12 00:19:00 +0300

    Adding new file

--- chapter03/homeworks/Homework27.java
+++ chapter03/homeworks/Homework27.java
@@ -1,5 +1,20 @@
 package chapter03.homeworks;
 
-public class Homework27 {
+import java.util.Scanner;
 
+public class Homework27 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		// Geometry: points in triangle
+
+		System.out.println("Enter a point's x- and y-coordinates: ");
+		double x = input.nextDouble();
+		double y = input.nextDouble();
+
+		if ((x == 0 && y <= 100) || (y == 0 && x <= 200) || (x <= 200 && y <= 100 && y / x < 0.5))
+			System.out.print("The point is in the triangle");
+		else
+			System.out.print("The point is not in the triangle.");	
+	}
 }
