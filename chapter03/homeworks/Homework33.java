84a3a7b1a26bb0add5084c659039b5b74ec38f4d
Gülşah <gulsahtuncay1@gmail.com>
2023-07-12 15:11:05 +0300

    Adding new file

--- chapter03/homeworks/Homework33.java
+++ chapter03/homeworks/Homework33.java
@@ -1,5 +1,24 @@
 package chapter03.homeworks;
 
-public class Homework33 {
+import java.util.Scanner;
 
+public class Homework33 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter weight and price for package 1: ");
+		System.out.println("Enter weight and price for package 2:");
+
+		double w1 = input.nextDouble();
+		double p1 = input.nextDouble();
+		double w2 = input.nextDouble();
+		double p2 = input.nextDouble();
+
+		if (p1 / w1 > p2 / w2)
+			System.out.println("Package 2 has a better price.");
+		else if (p1 / w1 < p2 / w2)
+			System.out.println("Package 1 has a better price.");
+		else
+			System.out.println("Two packages have the same price.");
+	}
 }
