bff050d2c0b4c1e19f93861be0421b4dbb7df0c4
Gülşah <gulsahtuncay1@gmail.com>
2023-07-12 15:11:39 +0300

    Adding new file

--- chapter03/homeworks/Homework34.java
+++ chapter03/homeworks/Homework34.java
@@ -1,5 +1,26 @@
 package chapter03.homeworks;
 
-public class Homework34 {
+import java.util.Scanner;
 
+public class Homework34 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter three points for p0, p1, and p2: ");
+		double x0 = input.nextDouble();
+		double y0 = input.nextDouble();
+		double x1 = input.nextDouble();
+		double y1 = input.nextDouble();
+		double x2 = input.nextDouble();
+		double y2 = input.nextDouble();
+
+		double line = (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0);
+
+		if (line == 0)
+			System.out
+					.println(x2 + "," + y2 + " is on the line segment from " + x0 + "," + y0 + " to " + x1 + "," + y1);
+		else if (line != 0)
+			System.out.println(
+					x2 + "," + y2 + " is not on the line segment from " + x0 + "," + y0 + " to " + x1 + "," + y1);
+	}
 }
